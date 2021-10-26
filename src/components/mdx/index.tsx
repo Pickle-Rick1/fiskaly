e281ba06e71388912580633769856ba1d2b35058
Matthias Grieder <matthias.grieder@snapscreen.com>
2021-10-26 20:55:25 +0200

    add typography content

--- src/components/mdx/index.tsx
+++ src/components/mdx/index.tsx
@@ -39,7 +39,7 @@ const components: MDXProviderComponentsProp = {
     const props = preToCodeBlock(preProps)
     // if there's a codeString and some props, we passed the test
     if (props) {
-      return <CodeBlock {...props} />
+      return <ColorsBlock {...props} />
     } else {
       // it's possible to have a pre without a code in it
       return <pre {...preProps} />
